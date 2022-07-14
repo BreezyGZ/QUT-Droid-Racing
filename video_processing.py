fb197e5b5cf17a09b3e95c621bbd18f7592affc9
Zoshi <shizongyan2002@gmail.com>
2022-07-14 22:00:54 +1000

    pain

--- video_processing.py
+++ video_processing.py
@@ -12,6 +12,19 @@ from functions import frameRescale, perspectiveShift, findAverageX, findMaxY, fi
 from obstacle_detection import obstacle_avoid_script
 from global_variables import BLUE_LOWER, BLUE_UPPER, YELLOW_LOWER, YELLOW_UPPER, BLACK_THRESHOLD, SIMILARITY_THRESHOLD, CONTOUR_AREA_THRESHOLD_BLACK, CONTOUR_AREA_THRESHOLD_LINE, PERSPECTIVE_SHIFT_COORDS, CONTOUR_LEFT, CONTOUR_RIGHT
 
+# Libraries and software controlling ultrasonic sensor
+import RPi.GPIO as GPIO
+#GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
+ 
+#set GPIO Pins
+GPIO_TRIGGER = 18
+GPIO_ECHO = 24
+ 
+#set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+
 camera = PiCamera()
 camera.resolution = (832, 624)
 camera.framerate = 32
@@ -32,7 +45,8 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     
     blue_mask = cv.inRange(hsv_img, BLUE_LOWER, BLUE_UPPER)
     yellow_mask = cv.inRange(hsv_img, YELLOW_LOWER, YELLOW_UPPER)
-
+    object_distance = distance()
+    print("Object distance: %.1f" % object_distance)
     # edge_yellow = findLargestContour(yellow_mask, CONTOUR_AREA_THRESHOLD_LINE)[0]
     # edge_blue = findLargestContour(blue_mask, CONTOUR_AREA_THRESHOLD_LINE)[0]
 
@@ -65,3 +79,31 @@ ser.write('kill\n'.encode('utf-8'))
 ser.flushInput()
 camera.close()
 # cv.destroyAllWindows()
+
+# finds the distance b/w the ultrasonic sensor and in front
+def distance():
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+ 
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+ 
+    StartTime = time.time()
+    StopTime = time.time()
+ 
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+ 
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+ 
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+ 
+    return distance
